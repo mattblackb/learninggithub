75935bd6b276e3e7b27078ed94145b27596cd731
Matt Burton <matt@plain.black>
2021-07-13 17:52:16 +0100

    Initial Commit - default project

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Matthew Burton on 13/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
